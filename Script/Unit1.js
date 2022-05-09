4ddf4952ee4d5a8babc48e2e8ea71b489661cb20
remi oum <roum@iom.int>
2022-05-09 14:42:19 +0800

    first commit after creating repo local

---
+++ Script/Unit1.js
@@ -0,0 +1,17 @@
+﻿function ClickCard()
+{
+if (KeywordTests.GroupOrder.Variables.OrderTable.ColumnName("F1") == 2)
+{ 
+  Aliases.browser.pageWebOrders2.formAspnetform.radiobuttonAmericanExpress.ClickButton();
+}
+
+if (KeywordTests.GroupOrder.Variables.OrderTable.ColumnName("F1") == 1)
+{ 
+  Aliases.browser.pageWebOrders2.formAspnetform.radiobuttonMastercard.ClickButton();
+}
+
+if (KeywordTests.GroupOrder.Variables.OrderTable.ColumnName("F1") == 0)
+{ 
+  Aliases.browser.pageProcess.formAspnetform.radiobuttonVisa.ClickButton();
+}
+}
